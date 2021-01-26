41a607ef9ca96dc426ea4c58cb89a459e683f22a
Bilal AMRI <iambilalamri@gmail.com>
2021-01-26 20:53:53 +0100

    [feat-collaborator]- build the models of api

--- src/main/java/amrib/collaborator/SpringCollaboratorApplication.java
+++ src/main/java/amrib/collaborator/SpringCollaboratorApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCollaboratorApplication {
-
+	/**
+	 * https://www.researchgate.net/figure/Class-diagram-for-a-management-employee-system_fig1_320246921
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCollaboratorApplication.class, args);
 	}
