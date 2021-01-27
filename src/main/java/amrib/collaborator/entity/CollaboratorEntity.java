0b026f61f93ebc39e61c95fdb235b492ea3f6583
Bilal AMRI <iambilalamri@gmail.com>
2021-01-27 21:11:38 +0100

    [feat-collaborator]- build the entities file of api

--- src/main/java/amrib/collaborator/entity/CollaboratorEntity.java
+++ src/main/java/amrib/collaborator/entity/CollaboratorEntity.java
@@ -1,5 +1,43 @@
 package amrib.collaborator.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "COLLABORATORS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CollaboratorEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstname;
+
+	@Column(name = "last_name")
+	private String lastname;
+
+	@Column(name = "email")
+	@NotBlank(message = "The email should not be blank")
+	private String email;
+
+	public CollaboratorEntity(String firstname, String lastname, String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
 
 }
