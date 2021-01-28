04a9ea4b863d0e66b0b8a00f9d5255d75a73fb80
Bilal AMRI <iambilalamri@gmail.com>
2021-01-28 18:22:21 +0100

    [feat-collaborator]- implement collaborator's controller

--- src/main/java/amrib/collaborator/controllers/CollaboratorController.java
+++ src/main/java/amrib/collaborator/controllers/CollaboratorController.java
@@ -1,10 +1,61 @@
 package amrib.collaborator.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import amrib.collaborator.entity.CollaboratorEntity;
+import amrib.collaborator.exception.ResourceNotFoundException;
+import amrib.collaborator.repository.CollaboratorRepository;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/collaborator")
 public class CollaboratorController {
+
+	@Autowired
+	private CollaboratorRepository collaboratorRepository;
+
+	@GetMapping(value = "/")
+	public List<CollaboratorEntity> getCollaborators() {
+		return collaboratorRepository.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public CollaboratorEntity getCollaborator(@PathVariable(value = "id") long id) {
+		return collaboratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Collaborator not found with id" + id));
+	}
+
+	@PostMapping(value = "/")
+	public CollaboratorEntity createCollaborator(@RequestBody CollaboratorEntity collaborator) {
+		return collaboratorRepository.save(collaborator);
+	}
+
+	@PutMapping(value = "/{id}")
+	public CollaboratorEntity updateCollaborator(@RequestBody CollaboratorEntity collaborator,
+			@PathVariable("id") long id) {
+		CollaboratorEntity existing = collaboratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Collaborator not found with id" + id));
+		existing.setFirstname(collaborator.getFirstname());
+		existing.setLastname(collaborator.getLastname());
+		existing.setEmail(collaborator.getEmail());
+		return this.collaboratorRepository.save(existing);
+	}
+
+	public ResponseEntity<CollaboratorEntity> deleteCollaborator(@PathVariable(value = "id") long id) {
+
+		CollaboratorEntity existing = collaboratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Collaborator not found with id" + id));
+		this.collaboratorRepository.delete(existing);
+		return ResponseEntity.ok().build();
+	}
 
 }
