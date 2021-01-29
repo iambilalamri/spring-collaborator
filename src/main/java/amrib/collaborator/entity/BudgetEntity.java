c13f2747a193bdfdc1a9b12bc3fa1871458a5463
Bilal AMRI <iambilalamri@gmail.com>
2021-01-29 23:10:47 +0100

    [feat-collaborator]- implement  controller errors

--- src/main/java/amrib/collaborator/entity/BudgetEntity.java
+++ src/main/java/amrib/collaborator/entity/BudgetEntity.java
@@ -2,11 +2,17 @@ package amrib.collaborator.entity;
 
 import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BudgetEntity {
 
 	private Long id;
+	private String type;
+	private String value;
+	private String description;
 }
--- src/main/java/amrib/collaborator/entity/ProjectEntity.java
+++ src/main/java/amrib/collaborator/entity/ProjectEntity.java
@@ -1,12 +1,18 @@
 package amrib.collaborator.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 
 import lombok.Data;
 
 @Data
 public class ProjectEntity {
-	
+
 	private Long id;
+	private String name;
+	private Date deadline;
+	private String budget;
+	private String sector;
 
 }
